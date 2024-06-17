f0118705a989f46826e1f94799fb3e9db59702c5
Cristina <cristina.velican@gmail.com>
2024-06-17 16:23:13 +0200

    Update the repo with the exercise of 17/06 (Create a DTO, Convert POJO to JSON and refactor the previous test)

--- src/test/java/builders/CoffeeBuilder.java
+++ src/test/java/builders/CoffeeBuilder.java
@@ -1,4 +1,15 @@
 package builders;
+
+import dto.Coffee;
+
 // create different Coffee instances
 public class CoffeeBuilder {
+
+    public static Coffee createCoffeeObj(int id, String name, String description){
+        Coffee coffee = new Coffee();
+        coffee.setName(name);
+        coffee.setId(id);
+        coffee.setDescription(description);
+        return coffee;
+    }
 }
--- src/test/java/restassured/CreateCoffeeTests.java
+++ src/test/java/restassured/CreateCoffeeTests.java
@@ -1,8 +1,12 @@
 package restassured;
+import builders.CoffeeBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dto.Coffee;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import support.SupportFunctions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,8 +36,19 @@ public class CreateCoffeeTests {
 
     // refactor this test
     @Test
-    void createCoffeeTestRefactor() {
-    
+    void createCoffeeTestRefactor() throws JsonProcessingException {
+        Coffee newCoffeeObject = CoffeeBuilder.createCoffeeObj(10, "Cristina's coffee","Best coffee in the city");
+        String jsonBody = SupportFunctions.convertJavaObjectToJson(newCoffeeObject);
+        System.out.println(jsonBody);
+        RestAssured.given()
+                 .body(jsonBody)
+                .accept(ContentType.TEXT)
+                .contentType(ContentType.JSON)
+                .post(BASE_URL)
+                .then()
+                .statusCode(200)
+                .body("name",equalTo("Cristina's coffee"))
+                .body(containsString("coffee"));
     }
 
 }
