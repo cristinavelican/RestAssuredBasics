a40e0ec77327d154a7366ee00ecc5567ad104ee1
Cristina <cristina.velican@gmail.com>
2024-07-04 21:26:34 +0200

    Add tests for Exception Handling

--- src/test/java/dto/Coffee.java
+++ src/test/java/dto/Coffee.java
@@ -13,23 +13,21 @@ public class Coffee {
 
     //setters and getters
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
+
+
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+
 }
--- src/test/java/restassured/CreateCoffeeTests.java
+++ src/test/java/restassured/CreateCoffeeTests.java
@@ -9,9 +9,14 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import support.SupportFunctions;
 
+
+import javax.crypto.spec.PSource;
+import java.sql.SQLOutput;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class CreateCoffeeTests {
 
     static final String BASE_URL = "https://webservice.toscacloud.com/api/v1/Coffees";
@@ -36,23 +41,59 @@ public class CreateCoffeeTests {
     }
 
     // refactor this test
-    @Test
-    void createCoffeeTestRefactor() throws JsonProcessingException {
-        //Coffee newCoffeeObject = CoffeeBuilder.createCoffeeObj(10, "Cristina's coffee","Best coffee in the city");
+    //@Test
+//    void createCoffeeTestRefactor() throws JsonProcessingException {
+//        //Coffee newCoffeeObject = CoffeeBuilder.createCoffeeObj(10, "Cristina's coffee","Best coffee in the city");
+//
+//        NormalCoffeeBuilder builder = new NormalCoffeeBuilder();
+//        Coffee newCoffeeObject = builder.setName("Cristina").setDescription("Cristina's coffee").setId(10).build();
+//        String jsonBody = SupportFunctions.convertJavaObjectToJson(newCoffeeObject);
+//        System.out.println("the coffee body is=");
+//        System.out.println(jsonBody);
+//        RestAssured.given()
+//                 .body(jsonBody)
+//                .accept(ContentType.TEXT)
+//                .contentType(ContentType.JSON)
+//                .post(BASE_URL)
+//                .then()
+//                .statusCode(200)
+//                .body("name",equalTo("Cristina"))
+//                .body(containsString("coffee"));
+//    }
 
+    // create a new test for PUT Coffees
+    @Test
+    void putCoffee()
+    {
         NormalCoffeeBuilder builder = new NormalCoffeeBuilder();
-        Coffee newCoffeeObject = builder.setName("Cristina").setDescription("Cristina's coffee").setId(10).build();
-        String jsonBody = SupportFunctions.convertJavaObjectToJson(newCoffeeObject);
-        System.out.println(jsonBody);
-        RestAssured.given()
-                 .body(jsonBody)
+        Coffee newCoffeeObject = builder.setId(4).setName("Monday Coffee").setDescription("Cristina's coffee").build();
+        String jsonCoffee = "";
+        try {
+             jsonCoffee = SupportFunctions.convertJavaObjectToJson(newCoffeeObject);
+        }
+        catch(JsonProcessingException e) {
+            System.out.println("There was a JSONProcessing Exception");
+        }
+        System.out.println("the body is:" + jsonCoffee);
+
+        Response response =  RestAssured.given()
+                .header("accept", "text/plain")
+                .header("Content-Type", "application/json")
                 .accept(ContentType.TEXT)
                 .contentType(ContentType.JSON)
-                .post(BASE_URL)
+                .body(jsonCoffee)
+                .when()
+                .put(BASE_URL);
+
+        RestAssured.given()
+                .accept(ContentType.TEXT)
+                .contentType(ContentType.JSON)
+                .body(jsonCoffee)
+                .when()
+                .put(BASE_URL)
                 .then()
                 .statusCode(200)
-                .body("name",equalTo("Cristina"))
-                .body(containsString("coffee"));
-    }
+                .body("name", equalTo("Monday Coffee"));
 
+    }
 }
